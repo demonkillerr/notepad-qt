df28e90b49af6512a40c3e98d1ead6897acdd6a0
demonkiller <gaurangvishwakarma619@gmail.com>
2023-09-13 03:48:28 +0530

    [FEAT] New, Save, Open

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,3 +14,44 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this, "Open The File");
+    QFile file(filename);
+    currentFile = filename;
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(filename);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+
+void MainWindow::on_actionSave_As_triggered()
+{
+    QString filename = QFileDialog::getSaveFileName(this, "Save As");
+    QFile file(filename);
+    if(!file.open(QIODevice::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    currentFile = filename;
+    setWindowTitle(filename);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
