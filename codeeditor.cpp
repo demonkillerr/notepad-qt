5bf43061cbd56ce2807281e8a33416e6023182f3
demonkiller <gaurangvishwakarma619@gmail.com>
2023-10-16 19:33:51 +0530

    initialise codeeditor constructor

--- codeeditor.cpp
+++ codeeditor.cpp
@@ -1,7 +1,16 @@
+#include <QtWidgets>
+#include <bits/stdc++.h>
+
 #include "codeeditor.h"
 
-CodeEditor::CodeEditor(QObject *parent)
-    : QObject{parent}
+CodeEditor::CodeEditor(QObject *parent) : QObject{parent}
 {
+    lineNumberArea = new LineNumberArea(this);
 
+    connect(this, &CodeEditor::blockCountChanged, this, &CodeEditor::updateLineNumberAreaWidth);
+    connect(this, &CodeEditor::updateRequest, this, &CodeEditor::updateLineNumberArea);
+    connect(this, &CodeEditor::cursorPositionChanged, this, &CodeEditor::highlightCurrentLine);
+
+    updateLineNumberAreaWidth(0);
+    highlightCurrentLine();
 }
